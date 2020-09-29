1b7f06698fff6840397dd7e4a400c58dc55e6ba8
Tomi1989 <tzivkovic89@gmail.com>
2020-09-29 11:56:49 +0200

    add list owners

--- Pet-Clinic-Data/src/main/java/services/OwnerService.java
+++ Pet-Clinic-Data/src/main/java/services/OwnerService.java
@@ -1,6 +1,5 @@
 package services;
 import guru.springframework.sfgpetclinic.model.Owner;
-import org.springframework.context.annotation.ComponentScan;
 
 public interface OwnerService extends CrudService<Owner, Long>{
     Owner findByLastName(String lastName);
