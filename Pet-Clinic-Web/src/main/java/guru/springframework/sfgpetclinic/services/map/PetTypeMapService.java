65c92bd6ed0dd48edbc96c438de1651bdd1a1990
Tomislav Zivkovic <tomislav.zivkovic@ericsson.com>
2020-10-06 12:57:22 +0200

    Pet Type Data on Startup

--- Pet-Clinic-Web/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeMapService.java
+++ Pet-Clinic-Web/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeMapService.java
@@ -1,9 +1,11 @@
 package guru.springframework.sfgpetclinic.services.map;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
 
     @Override
