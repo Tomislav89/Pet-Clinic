2f8e67af84096238288e96cb7cc7ed60d4d73ae7
Tomislav Zivkovic <tomislav.zivkovic@ericsson.com>
2020-10-30 07:57:55 +0100

    add visits jpa

--- Pet-Clinic-Data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ Pet-Clinic-Data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -21,14 +23,15 @@ public class Pet extends BaseEntity{
     @JoinColumn(name = "type_id")
     private PetType petType;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public PetType getPetType() {
         return petType;
     }
@@ -46,5 +49,13 @@ public class Pet extends BaseEntity{
     }
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
