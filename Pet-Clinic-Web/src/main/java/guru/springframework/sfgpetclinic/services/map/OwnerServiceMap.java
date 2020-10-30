11af9c784672346dd37f4edf87137a14648109a5
Tomislav Zivkovic <tomislav.zivkovic@ericsson.com>
2020-10-30 12:09:13 +0100

    add owners jpa service -> error included

--- Pet-Clinic-Web/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ Pet-Clinic-Web/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -4,6 +4,7 @@ import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.services.PetService;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import java.util.Set;
