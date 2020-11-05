c56347bb7afd893924604e83b6f2edf375cda4f4
Tomi1989 <tzivkovic89@gmail.com>
2020-11-05 11:59:26 +0100

    add visit jpa service

--- Pet-Clinic-Web/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/VetSDJpaService.java
+++ Pet-Clinic-Web/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/VetSDJpaService.java
@@ -1,8 +1,6 @@
 package guru.springframework.sfgpetclinic.services.springdatajpa;
-import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.repositories.VetRepository;
-import guru.springframework.sfgpetclinic.services.OwnerService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
