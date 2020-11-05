078e1efcc70c019679206e152e7785f6f039e343
Tomi1989 <tzivkovic89@gmail.com>
2020-11-05 11:36:03 +0100

    Add Map Based Visit Service

--- Pet-Clinic-Web/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
+++ Pet-Clinic-Web/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -2,8 +2,11 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Visit;
 import guru.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
 import java.util.Set;
 
+@Service
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
 
     @Override
